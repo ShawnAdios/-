259168b8619234a48073f6455dea6f4f70eaf90f
Math <oldgreece@qq.com>
2024-07-11 15:23:19 +0800

    1

--- basic/main.cpp
+++ basic/main.cpp
@@ -1,9 +1,6 @@
-
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-
-    
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main() {
+    cout << "Hello, World!\n";
     return 0;
 }
