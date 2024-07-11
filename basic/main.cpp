069a3d2eb9dfd7876ed7ac05e62fc5ddab83d235
Math <oldgreece@qq.com>
2024-07-11 13:12:11 +0800

    Initial Commit

---
+++ basic/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  basic
+//
+//  Created by Math on 2024/7/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
